869defb4a0f74ba738e85c4d25a5b18860d9c56a
team <teamuncopyrightable@gmail.com>
2018-03-27 19:55:37 -0700

    added javadoc

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SideIndependentAuto.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class SideIndependentAuto extends LinearOpMode {
+    private ElapsedTime     runtime = new ElapsedTime();
+
+    /**
+     * Runs ingenious autonomous code that scores on average #{Game.MAX_AUTONOMOUS_POINTS} points
+     * @precondion  Robot Works
+     * @postcondion Win Competition
+     */
+    @Override
+    public void runOpMode() {
+
+    }
+}
